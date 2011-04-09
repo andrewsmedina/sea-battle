1f2cef020403574e6c3a42b7739ce4858ce783f4
andrews medina <andrewsmedina@gmail.com>
2011-04-09 00:07:07 -0300

    implementing shoot

--- battle/game.py
+++ battle/game.py
@@ -10,8 +10,15 @@ class EventLayer(Layer):
 
     is_event_handler = True
 
-    def on_mouse_press(self, *args):
-        print 'FOO'
+    def __init__(self, matrix):
+        super(EventLayer, self).__init__()
+        self.matrix = matrix
+
+    def on_mouse_press(self, x, y, *args):
+        for line in self.matrix:
+            for sprite in line:
+                if (x > sprite.position[0]-25) and (x < sprite.position[0]+25) and (y > sprite.position[1]-25) and (y < sprite.position[1]+25):
+                    sprite.visible = False
 
 class Ship(Sprite):
 
@@ -25,21 +32,27 @@ class Game(Scene):
     def __init__(self):
         super(Game, self).__init__()
 
-        self.add(EventLayer())
-
         self.build_game_board()
+
+        self.add(EventLayer(self.matrix))
 
         ship = Ship()
         ship.position = (100, 75)
         self.add(ship, z=100)
 
     def build_game_board(self):
-
+        self.matrix = []
         for x in range(10):
+            line = []
+
             for y in range(10):
                 square = Sprite('square.png')
                 square.position = (75 + (50 * x), 75 + (50 * y))
                 self.add(square)
+
+                line.append(square)
+
+            self.matrix.append(line)
 
         self.line_labels()
         self.column_labels()
