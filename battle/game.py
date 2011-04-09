fc29558e193f378b1137c2501b7ce7a1acd4a631
andrews medina <andrewsmedina@gmail.com>
2011-04-08 21:55:48 -0300

    changing initial game board position

--- battle/game.py
+++ battle/game.py
@@ -13,5 +13,5 @@ class Game(Scene):
         for x in range(10):
             for y in range(10):
                 square = Sprite('square.png')
-                square.position = (25 + (50 * x), 25 + (50 * y))
+                square.position = (75 + (50 * x), 75 + (50 * y))
                 self.add(square)
