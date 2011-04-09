05509c231da0101c334e1f0e693ab15c4a509472
andrews medina <andrewsmedina@gmail.com>
2011-04-08 21:52:33 -0300

    building a game board

--- battle/game.py
+++ battle/game.py
@@ -1,8 +1,17 @@
 from cocos.scene import Scene
+from cocos.sprite import Sprite
 
 class Game(Scene):
 
     def __init__(self):
         super(Game, self).__init__()
 
+        self.build_board()
 
+    def build_board(self):
+
+        for x in range(10):
+            for y in range(10):
+                square = Sprite('square.png')
+                square.position = (25 + (50 * x), 25 + (50 * y))
+                self.add(square)
